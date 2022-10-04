6cd5068c2f99bd69a77ae330d8f926170d84ba54
rlawnaks <rlawnaks1234@naver.com>
2022-10-04 20:55:41 +0900

    final

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express"),
-    app = express()
+    app = express(),
     pool = require("./config/database")
     
 app.use(express.json())
 app.use(express.urlencoded({extended : false }))
 
 app.get('/', (req, res) => {
-    res.json({ result: true })
+    res.json('Hello World')
+})
+
+app.post('/', (req, res) => {
+    const {name, age} = req.body
+    pool((conn) => {
+        conn.query("insert into tbl_user(name, age) values(? ,?)", [name, age], (err, doc) => {
+            if (err) console.log(err)
+            res.json({result: true})
+        })
+        conn.release();
+    })
+    
 })
 
 const port = 3030;
---
+++ tabel.sql
@@ -0,0 +1,5 @@
+create table tbl_user (
+    name varchar(20) not null,
+    age integer not null,
+    idx integer auto_increment primary key
+)
