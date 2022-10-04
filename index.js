aa45a09f536488bca0c3ab269e6da6f15817d5dd
rlawnaks <rlawnaks1234@naver.com>
2022-10-04 20:34:08 +0900

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser");
+    app = express()
+    pool = require("./config/database")
+    
+app.use(express.json())
+app.use(express.urlencoded({extended : false }))
 
+app.get('/', (req, res) => {
+    res.json({ result: true })
+})
 
-
-const port = 8000;
+const port = 3030;
 app.listen(port, () => {
     console.log(`SERVER ON! PORT : ${port}`)
 })
